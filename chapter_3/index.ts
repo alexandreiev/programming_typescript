96b375a8f4208677401f1dede05e6cec3e687397
oleksandr <oleksandr.andreiev@spacelabs.com>
2022-05-26 23:08:45 +0300

    index has been added

---
+++ chapter_3/index.ts
@@ -0,0 +1,13 @@
+// function squareOf(n: any) {
+//     return n * n
+// }
+// console.log(squareOf(2))     // evaluates to 4
+// console.log(squareOf('z'))   // evaluates to Nan
+
+// function squareOf(n: number) {
+//     return n * n
+// }
+// console.log(squareOf(2))     // evaluates to 4
+// console.log(squareOf('z'))   // Error T2345: Argument of type '"z"' is not assignable to
+                                // parameter of type 'number'
+
