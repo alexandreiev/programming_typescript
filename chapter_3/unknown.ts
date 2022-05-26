68a68a05956c384b2f7ff149c87dd653996a63e8
oleksandr <oleksandr.andreiev@spacelabs.com>
2022-05-26 23:10:11 +0300

    number, string and unknown have been added

---
+++ chapter_3/unknown.ts
@@ -0,0 +1,9 @@
+let a: unknown = 30         // unknow
+let b = a === 123           // boolean
+let c = a + 10              // Error TS2571: Object is of type 'unknown'
+if (typeof a === 'number') {
+    let d = a + 10          // number
+}
+
+
+export{}
