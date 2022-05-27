9ff2fab82e5979fb2e041c06291de832c2dcce3c
oleksandr.andreiv <oleksandr.andreiev@globallogic.com>
2022-05-27 16:11:39 +0300

    nullUndefinedVoidNever has been added

---
+++ chapter_3/nullUndefinedVoidNever.ts
@@ -0,0 +1,40 @@
+// (a) A function that returns a number or null
+function a(x: number) {
+    if (x < 10) {
+        return x
+    }
+    return null
+}
+
+// (b) A function that returns underfined
+function b(){
+    return undefined
+}
+
+// (c) A function that returns void
+function c() {
+    let a = 2 + 2
+    let b = a * a
+}
+
+// (d) A function that returns never
+function d() {
+    throw TypeError('I always error')
+}
+
+// (e) Another function that returns never
+function e() {
+    while (true) {
+        doSomething()
+    }
+}
+
+// strict null Checking
+function addDelicioysFish(pizza: Pizza) {
+    return pizza.addAnchovies() // Uncaught TypeError: Cannot read
+                                // property 'addAnchovies' of null
+}
+// TypeScript lets this fly with strictNullChecks = false
+addDelicioysFish(null)
+
+export{}
