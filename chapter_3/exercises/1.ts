91f68ebd0fa1af7ef04f37d4138c704d05c6206c
oleksandr <oleksandr.andreiev@spacelabs.com>
2022-05-28 22:33:53 +0300

    examples have been added

---
+++ chapter_3/exercises/1.ts
@@ -0,0 +1,12 @@
+// For each of these values, what type will TypeScript infer?
+
+let a = 10.42
+let b = 'apples and oranges'
+const c = 'pineapples'
+let d = [true, true, false]
+let e = {type: 'ficus'}
+let f = [1, false]
+const g = [3]
+let h = null
+
+export{}
