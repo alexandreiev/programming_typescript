abaa52f8a512b78662afc1176299a4a2b2c6048a
oleksandr.andreiv <oleksandr.andreiev@globallogic.com>
2022-06-03 22:20:25 +0300

    whenAreGenericBound and whereCanYouDeclateGenerics.ts have been added

---
+++ chapter_4/whenAreGenericsBound.ts
@@ -0,0 +1,22 @@
+type Filter = {
+    <T>(array: T[], f: (item: T) => boolean): T[]
+}
+
+let filter: Filter = (array, f) => [] 
+    
+
+type Filter1<T> = {
+    (array: T[], f: (item: TemplateStringsArray) => boolean): T[]
+}
+
+let filter1: Filter1 = (array, f) => []     // Error TS2314: Generic type 'Filter1'
+                                            // requires 1 type argument(s).
+
+type Otherfilter = Filter1  // Error TS2314: Generic type 'Filter1'
+                            // requires 1 type argument(s).
+
+let filter2: Filter1<number> = (array, f) => []
+type StringFilter = Filter1<String>
+let stringFilter: StringFilter = (array, f) => []
+
+export {}
