fd1415258429126f4e38e1d058bcdbabfa4c2a04
oleksandr <oleksandr.andreiev@spacelabs.com>
2022-06-11 22:37:33 +0300

    genericTypeDEfaults has been added

---
+++ chapter_4/genericTypeDefaults.ts
@@ -0,0 +1,36 @@
+type MyEvent<T = HTMLElement> = {
+    target: T
+    type: string
+}
+
+type MyEvent1<T extends HTMLElement> = {
+    target: T
+    type: string
+}
+
+
+let buttonEvent1: MyEvent<HTMLButtonElement> = {
+    target: myElement,
+    type: string
+}
+
+
+// Good
+type Myevent2<
+    Type extends string,
+    Target extends HTMLElement = HTMLElement,
+> = {
+    targer: Target
+    type: Type
+} 
+
+// Bad
+type MyEvent3<
+Target extends HTMLElement = HTMLElement,
+Type extends string     // Error TS2706: Required type parameters may
+> = {                   // not follow optional type parameters.
+    target: Target
+    type: Type
+}
+
+export {}
